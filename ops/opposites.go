8f88858a23612e389de3c5cd1c1b71c38bb2a792
luismasuelli <luis@triplemint.com>
2019-06-11 11:49:33 -0500

    opposite

---
+++ ops/opposites.go
@@ -0,0 +1,38 @@
+package ops
+
+import (
+	"github.com/universe-10th-calculus/sets"
+	"math/big"
+)
+
+
+var oneInt = big.NewInt(1)
+var oneFloat = big.NewFloat(1)
+
+
+func Neg(a sets.Number) sets.Number {
+	switch va := a.(type) {
+	case *big.Int:
+		return big.NewInt(0).Neg(va)
+	case *big.Rat:
+		return big.NewRat(0, 1).Neg(va)
+	case *big.Float:
+		return big.NewFloat(0).Neg(va)
+	default:
+		panic("cannot negate a non-*big.(Int, Float, Rat) value")
+	}
+}
+
+
+func Inv(a sets.Number) sets.Number {
+	switch va := a.(type) {
+	case *big.Int:
+		return big.NewRat(0, 1).SetFrac(oneInt, va)
+	case *big.Rat:
+		return big.NewRat(0, 1).Inv(va)
+	case *big.Float:
+		return big.NewFloat(0).Quo(oneFloat, va)
+	default:
+		panic("cannot negate a non-*big.(Int, Float, Rat) value")
+	}
+}
