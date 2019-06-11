5d1eaaf1bbf4b02c3a8fc9de2683fbb00fb3ba0e
luismasuelli <luis@triplemint.com>
2019-06-11 16:15:57 -0500

    function now supports arguments

--- expressions/function.go
+++ expressions/function.go
@@ -1,8 +1,15 @@
 package diff
 
+import (
+	"strings"
+	"fmt"
+)
+
 
 type Function interface {
+	fmt.Stringer
 	StandardName() string
+	Arguments()    []Expression
 }
 
 
@@ -13,4 +20,25 @@ type FunctionExpr struct {
 
 func (function FunctionExpr) StandardName() string {
 	return function.standardName
+}
+
+
+func (function FunctionExpr) Arguments() []Expression {
+	panic("not implemented")
+}
+
+
+func (function FunctionExpr) String() string {
+	arguments := function.Arguments()
+	placeholders := make([]string, len(arguments))
+	for index := range placeholders {
+		placeholders[index] = "%s"
+	}
+	fmtString := "%s(" + strings.Join(placeholders, ", ") + ")"
+	fmtArgs := make([]interface{}, len(arguments) + 1)
+	fmtArgs[0] = function.StandardName()
+	for index, argument := range arguments {
+		fmtArgs[index + 1] = argument
+	}
+	return fmt.Sprintf(fmtString, fmtArgs...)
 }
