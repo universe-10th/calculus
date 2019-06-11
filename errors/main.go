5f85a5426fd2bb37e8dfdd1af7c75c6efa74acdf
luismasuelli <luis@triplemint.com>
2019-06-11 17:20:06 -0500

    Adding more errors

--- errors/main.go
+++ errors/main.go
@@ -2,7 +2,10 @@ package errors
 
 import "errors"
 
-var LogarithmOfNegative = errors.New("attempted to calculate logarithm of negative number")
-var PowerOfNonPositive  = errors.New("attempted to calculate an invalid power (e.g. 0^0, x^y where x < 0)")
-var DivisionByZero      = errors.New("attempted to calculate a division by 0")
-var TangentOfVertical   = errors.New("attempted to calculate the tangent of a vertical angle")
+var UndefinedValue         = errors.New("one or more parameters are undefined")
+var LogarithmOfNegative    = errors.New("attempted to calculate logarithm of negative number")
+var PowerOfNonPositive     = errors.New("attempted to calculate an invalid power (e.g. 0^0, x^y where x < 0)")
+var DivisionByZero         = errors.New("attempted to calculate a division by 0")
+var TangentOfVertical      = errors.New("attempted to calculate the tangent of a vertical angle")
+// For derivative
+var NotDerivableExpression = errors.New("this expression is not derivable")
