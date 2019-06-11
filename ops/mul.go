54d95f87f1fc11f3648d18a588d39416998ef580
luismasuelli <luis@triplemint.com>
2019-06-11 12:36:39 -0500

    Mul/Div operations

---
+++ ops/mul.go
@@ -0,0 +1,68 @@
+package ops
+
+import (
+	"github.com/universe-10th-calculus/sets"
+	"math/big"
+)
+
+
+func mul(a, b, zero sets.Number) bool {
+	switch va := a.(type) {
+	case *big.Int:
+		if va.Cmp(zero.(*big.Int)) == 0 {
+			return true
+		}
+		va.Mul(va, b.(*big.Int))
+	case *big.Rat:
+		if va.Cmp(zero.(*big.Rat)) == 0 {
+			return true
+		}
+		va.Mul(va, b.(*big.Rat))
+	case *big.Float:
+		if va.Cmp(zero.(*big.Float)) == 0 {
+			return true
+		}
+		va.Mul(va, b.(*big.Float))
+	}
+	return false
+}
+
+
+func Mul(terms ...sets.Number) sets.Number {
+	if terms == nil {
+		return nil
+	} else if len(terms) == 1 {
+		return terms[0]
+	}
+	set := sets.BroaderAll(sets.ClosestAll(terms...)...)
+	zero := Zero(set)
+	current := One(set)
+	terms = sets.UpCastTo(set, terms...)
+	for _, term := range terms {
+		if mul(current, term, zero) {
+			return zero
+		}
+	}
+	return current
+}
+
+
+func Div(dividend sets.Number, dividers ...sets.Number) sets.Number {
+	if dividers == nil {
+		return dividend
+	}
+	divider := Add(dividers...)
+	set := sets.BroaderAll(sets.ClosestAll(dividend, divider)...)
+	cast := sets.UpCastTo(set, dividend, divider)
+	dividend = cast[0]
+	divider = cast[1]
+	switch vm := dividend.(type) {
+	case *big.Int:
+		return big.NewRat(0, 1).SetFrac(vm, divider.(*big.Int))
+	case *big.Rat:
+		return vm.Quo(vm, divider.(*big.Rat))
+	case *big.Float:
+		return vm.Quo(vm, divider.(*big.Float))
+	}
+	return nil
+}
