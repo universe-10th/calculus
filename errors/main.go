530a4582ea00dd13a1c41c18a8e17c8ce2a9524f
luismasuelli <luis@triplemint.com>
2019-06-11 18:01:16 -0500

    forgot to add! factorial error

--- errors/main.go
+++ errors/main.go
@@ -2,10 +2,11 @@ package errors
 
 import "errors"
 
-var UndefinedValue         = errors.New("one or more parameters are undefined")
-var LogarithmOfNegative    = errors.New("attempted to calculate logarithm of negative number")
-var PowerOfNonPositive     = errors.New("attempted to calculate an invalid power (e.g. 0^0, x^y where x < 0)")
-var DivisionByZero         = errors.New("attempted to calculate a division by 0")
-var TangentOfVertical      = errors.New("attempted to calculate the tangent of a vertical angle")
+var UndefinedValue           = errors.New("one or more parameters are undefined")
+var LogarithmOfNegative      = errors.New("attempted to calculate logarithm of negative number")
+var PowerOfNonPositive       = errors.New("attempted to calculate an invalid power (e.g. 0^0, x^y where x < 0)")
+var DivisionByZero           = errors.New("attempted to calculate a division by 0")
+var TangentOfVertical        = errors.New("attempted to calculate the tangent of a vertical angle")
+var InvalidFactorialArgument = errors.New("argument to factorial does not fit in a 64bit unsigned integer")
 // For derivative
-var NotDerivableExpression = errors.New("this expression is not derivable")
+var NotDerivableExpression   = errors.New("this expression is not derivable")
