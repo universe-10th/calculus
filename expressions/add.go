6be423fc630277948625ec159c170a4f28df4513
luismasuelli <luis@triplemint.com>
2019-06-11 22:51:15 -0500

    Add and Mul, without stringification

---
+++ expressions/add.go
@@ -0,0 +1,70 @@
+package expressions
+
+import (
+	"github.com/universe-10th-calculus/sets"
+	"github.com/universe-10th-calculus/ops"
+)
+
+
+type AddExpr struct {
+	terms []Expression
+}
+
+
+func (add *AddExpr) Evaluate(args Arguments) (sets.Number, error) {
+	terms := make([]sets.Number, len(add.terms))
+	for index, term := range add.terms {
+		if evaluated, err := term.Evaluate(args); err != nil {
+			return nil, err
+		} else {
+			terms[index] = evaluated
+		}
+	}
+	return ops.Add(terms...), nil
+}
+
+
+func (add *AddExpr) Derivative(wrt Variable) (Expression, error) {
+	derivatedTerms := make([]Expression, len(add.terms))
+	for index, term := range add.terms {
+		if derivatedTerm, err := term.Derivative(wrt); err != nil {
+			return nil, err
+		} else {
+			derivatedTerms[index] = derivatedTerm
+		}
+	}
+	return Add(derivatedTerms...), nil
+}
+
+
+func (add *AddExpr) CollectVariables(variables Variables) {
+	for _, term := range add.terms {
+		term.CollectVariables(variables)
+	}
+}
+
+
+func (add *AddExpr) String() string {
+	// TODO
+	return ""
+}
+
+
+func flattenTerms(terms []Expression) []Expression {
+	flattenedTerms := make([]Expression, 2)
+	for _, term := range terms {
+		if addExpr, ok := term.(*AddExpr); ok {
+			for _, term := range flattenTerms(addExpr.terms) {
+				flattenedTerms = append(flattenedTerms, term)
+			}
+		} else {
+			flattenedTerms = append(flattenedTerms, term)
+		}
+	}
+	return flattenedTerms
+}
+
+
+func Add(terms ...Expression) *AddExpr {
+	return &AddExpr{flattenTerms(terms)}
+}
---
+++ expressions/mul.go
@@ -0,0 +1,84 @@
+package expressions
+
+import (
+	"github.com/universe-10th-calculus/sets"
+	"github.com/universe-10th-calculus/ops"
+)
+
+
+type MulExpr struct {
+	factors []Expression
+}
+
+
+func (mul *MulExpr) Evaluate(args Arguments) (sets.Number, error) {
+	factors := make([]sets.Number, len(mul.factors))
+	for index, term := range mul.factors {
+		if evaluated, err := term.Evaluate(args); err != nil {
+			return nil, err
+		} else {
+			factors[index] = evaluated
+		}
+	}
+	return ops.Mul(factors...), nil
+}
+
+
+func (mul *MulExpr) Derivative(wrt Variable) (Expression, error) {
+	factors := make([]Expression, len(mul.factors))
+	for index, factor := range mul.factors {
+		factors[index] = factor
+	}
+	derivatedFactors := make([]Expression, len(mul.factors))
+	for index, factor := range factors {
+		if derivatedFactor, err := factor.Derivative(wrt); err != nil {
+			derivatedFactors[index] = derivatedFactor
+		}
+	}
+	terms := make([]Expression, len(mul.factors))
+	for index := range terms {
+		termFactors := make([]Expression, len(mul.factors))
+		for index2 := range terms {
+			if index == index2 {
+				termFactors[index2] = derivatedFactors[index2]
+			} else {
+				termFactors[index2] = factors[index2]
+			}
+		}
+		terms[index] = Mul(termFactors...)
+	}
+	return Add(terms...), nil
+}
+
+
+func (mul *MulExpr) CollectVariables(variables Variables) {
+	for _, term := range mul.factors {
+		term.CollectVariables(variables)
+	}
+}
+
+
+func (mul *MulExpr) String() string {
+	// TODO
+	return ""
+}
+
+
+func flattenFactors(factors []Expression) []Expression {
+	flattenedFactors := make([]Expression, 2)
+	for _, term := range factors {
+		if mulExpr, ok := term.(*MulExpr); ok {
+			for _, term := range flattenFactors(mulExpr.factors) {
+				flattenedFactors = append(flattenedFactors, term)
+			}
+		} else {
+			flattenedFactors = append(flattenedFactors, term)
+		}
+	}
+	return flattenedFactors
+}
+
+
+func Mul(terms ...Expression) *AddExpr {
+	return &AddExpr{flattenFactors(terms)}
+}
