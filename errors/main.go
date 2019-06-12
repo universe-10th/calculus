6d018c1dfb3b8a0eb5cff2cb257f23a2a3a937ce
luismasuelli <luis@triplemint.com>
2019-06-12 00:02:38 -0500

    Invalid power error

--- errors/main.go
+++ errors/main.go
@@ -4,7 +4,7 @@ import "errors"
 
 var UndefinedValue           = errors.New("one or more parameters are undefined")
 var LogarithmOfNegative      = errors.New("attempted to calculate logarithm of negative number")
-var PowerOfNonPositive       = errors.New("attempted to calculate an invalid power (e.g. 0^0, x^y where x < 0)")
+var InvalidPowerOperation    = errors.New("attempted to calculate an invalid power (e.g. 0^0, x^y where x < 0)")
 var DivisionByZero           = errors.New("attempted to calculate a division by 0")
 var TangentOfVertical        = errors.New("attempted to calculate the tangent of a vertical angle")
 var InvalidFactorialArgument = errors.New("argument to factorial does not fit in a 64bit unsigned integer")
