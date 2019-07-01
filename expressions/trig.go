5f141f521aa509af80ddef2686319916f2f6d8b2
luismasuelli <luis@triplemint.com>
2019-07-01 00:30:53 -0500

    Trigonometric functions calculation.

--- expressions/trig.go
+++ expressions/trig.go
@@ -6,47 +6,60 @@ import (
 	"github.com/universe-10th-calculus/errors"
 )
 
+
+// TrigFunctionExpr is a structure providing some helpers over simple FunctionExpr structs.
+// As an example, only one argument is used for these functions.
 type TrigFunctionExpr struct {
 	FunctionExpr
 	arg   Expression
 }
 
 
+// String returns the standard string representation of these functions.
+// They have one argument and their names are sin, cos, tan.
 func (trigFunctionExpr TrigFunctionExpr) String() string {
 	return FunctionDisplay(trigFunctionExpr)
 }
 
 
+// Arguments builds a list consisting on the single argument.
 func (trig TrigFunctionExpr) Arguments() []Expression {
 	return []Expression{ trig.arg }
 }
 
 
+// CollectVariables digs into the single argument expression.
 func (trig TrigFunctionExpr) CollectVariables(variables Variables) {
 	trig.arg.CollectVariables(variables)
 }
 
 
+// IsConstant evaluates whether the single argument expression is constant with respect to the given variable.
 func (trig TrigFunctionExpr) IsConstant(wrt Variable) bool {
 	return trig.arg.IsConstant(wrt)
 }
 
 
+// SinExpr stands for a sine expression.
 type SinExpr struct {
 	TrigFunctionExpr
 }
 
 
+// CosExpr stands for a cosine expression.
 type CosExpr struct {
 	TrigFunctionExpr
 }
 
 
+// TanExpr stands for a tangent expression.
 type TanExpr struct {
 	TrigFunctionExpr
 }
 
 
+// Simplify attempts reducing a sine expression to a constant.
+// It first simplifies the argument and, if it turns to be constant, returns a constant expression with its sine.
 func (sin SinExpr) Simplify() (Expression, error) {
 	if simplified, err := sin.arg.Simplify(); err != nil {
 		return nil, err
@@ -58,6 +71,7 @@ func (sin SinExpr) Simplify() (Expression, error) {
 }
 
 
+// Evaluate computes the sine expression by first computing the inner expression, and then applying the sine.
 func (sin SinExpr) Evaluate(args Arguments) (sets.Number, error) {
 	if result, err := sin.arg.Evaluate(args); err == nil {
 		return ops.Sin(result), nil
@@ -67,6 +81,7 @@ func (sin SinExpr) Evaluate(args Arguments) (sets.Number, error) {
 }
 
 
+// Derivative uses the sine rule and also applies the chain rule.
 func (sin SinExpr) Derivative(wrt Variable) (Expression, error) {
 	if derivative, err := sin.arg.Derivative(wrt); err != nil {
 		return nil, err
@@ -76,6 +91,8 @@ func (sin SinExpr) Derivative(wrt Variable) (Expression, error) {
 }
 
 
+// Simplify attempts reducing a cosine expression to a constant.
+// It first simplifies the argument and, if it turns to be constant, returns a constant expression with its sine.
 func (cos CosExpr) Simplify() (Expression, error) {
 	if simplified, err := cos.arg.Simplify(); err != nil {
 		return nil, err
@@ -87,6 +104,7 @@ func (cos CosExpr) Simplify() (Expression, error) {
 }
 
 
+// Evaluate computes the cosine over the evaluated value of the inner argument.
 func (cos CosExpr) Evaluate(args Arguments) (sets.Number, error) {
 	if result, err := cos.arg.Evaluate(args); err == nil {
 		return ops.Cos(result), nil
@@ -96,6 +114,7 @@ func (cos CosExpr) Evaluate(args Arguments) (sets.Number, error) {
 }
 
 
+// Derivative applies the cosine rule and also the chain rule.
 func (cos CosExpr) Derivative(wrt Variable) (Expression, error) {
 	if derivative, err := cos.arg.Derivative(wrt); err != nil {
 		return nil, err
@@ -117,6 +136,8 @@ func (tan TanExpr) wrappedTan(input sets.Number) (result sets.Number, err error)
 }
 
 
+// Simplify attempts reducing a tangent expression to a constant.
+// It first simplifies the argument and, if it turns to be constant, returns a constant expression with its sine.
 func (tan TanExpr) Simplify() (Expression, error) {
 	if simplified, err := tan.arg.Simplify(); err != nil {
 		return nil, err
@@ -132,6 +153,7 @@ func (tan TanExpr) Simplify() (Expression, error) {
 }
 
 
+// Evaluate computes the tangent over the evaluated value of the inner expression.
 func (tan TanExpr) Evaluate(args Arguments) (sets.Number, error) {
 	if result, err := tan.arg.Evaluate(args); err == nil {
 		return tan.wrappedTan(result)
@@ -141,6 +163,7 @@ func (tan TanExpr) Evaluate(args Arguments) (sets.Number, error) {
 }
 
 
+// Derivative uses the tangent rule (actually, the derivative of sin(x)/cos(x)) and also applies the chain rule.
 func (tan TanExpr) Derivative(wrt Variable) (Expression, error) {
 	if derivative, err := tan.arg.Derivative(wrt); err != nil {
 		return nil, err
@@ -150,16 +173,19 @@ func (tan TanExpr) Derivative(wrt Variable) (Expression, error) {
 }
 
 
+// Sin construct a sine expression.
 func Sin(arg Expression) Expression {
 	return SinExpr{TrigFunctionExpr{FunctionExpr{"sin"},arg}}
 }
 
 
+// Cos constructs a cosine expression.
 func Cos(arg Expression) Expression {
 	return CosExpr{TrigFunctionExpr{FunctionExpr{"cos"},arg}}
 }
 
 
+// Tan constructs a tangent expression.
 func Tan(arg Expression) Expression {
 	return TanExpr{TrigFunctionExpr{FunctionExpr{"tan"},arg}}
 }
