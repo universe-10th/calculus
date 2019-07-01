e1b89cda363fe8da227e0299d8373d4aed061a5b
luismasuelli <luis@triplemint.com>
2019-07-01 00:10:27 -0500

    Pow documentation.

--- expressions/pow.go
+++ expressions/pow.go
@@ -8,6 +8,7 @@ import (
 )
 
 
+// PowExpr stands both for roots and for powers. It will have a base and an exponent inner terms.
 type PowExpr struct {
 	base     Expression
 	exponent Expression
@@ -26,6 +27,8 @@ func (pow PowExpr) wrappedPow(base, exponent sets.Number) (result sets.Number, e
 }
 
 
+// Evaluate computes the power of the expression considering base and exponent.
+// Base expression and exponent expression are first evaluated.
 func (pow PowExpr) Evaluate(args Arguments) (sets.Number, error) {
 	if base, err := pow.base.Evaluate(args); err != nil {
 		return nil, err
@@ -85,6 +88,8 @@ func (pow PowExpr) derivativeBySpecialCases(
 }
 
 
+// Derivative uses one of the power rules of the derivative: f(x)^a, a^f(x), f(x)^g(x).
+// It also applies the chain rule appropriately over both functions.
 func (pow PowExpr) Derivative(wrt Variable) (Expression, error) {
 	// Say we have f(x), g(x)
 	// d[f(x)^g(x)]/dx = f(x)^(g(x)-1) * [g(x)*df(x)/dx + f(x)*ln(f(x))*dg(x)/dx]
@@ -101,17 +106,21 @@ func (pow PowExpr) Derivative(wrt Variable) (Expression, error) {
 }
 
 
+// CollectVariables digs into base and exponent expressions.
 func (pow PowExpr) CollectVariables(variables Variables) {
 	pow.base.CollectVariables(variables)
 	pow.exponent.CollectVariables(variables)
 }
 
 
+// IsConstant returns whether both base and exponent are constant with respect to the given value.
 func (pow PowExpr) IsConstant(wrt Variable) bool {
 	return pow.base.IsConstant(wrt) && pow.exponent.IsConstant(wrt)
 }
 
 
+// Simplify attempts a constant simplification over both base and exponent.
+// If both operands are constant, then it calculates the power into a new constant.
 func (pow PowExpr) Simplify() (Expression, error) {
 	if simplifiedBase, err := pow.base.Simplify(); err != nil {
 		return nil, err
@@ -135,6 +144,7 @@ func (pow PowExpr) Simplify() (Expression, error) {
 }
 
 
+// String represents the power appropriately: (X)^(Y), perhaps removing parentheses appropriately.
 func (pow PowExpr) String() string {
 	baseStr := pow.base.String()
 	if _, ok := pow.base.(SelfContained); !ok {
@@ -150,6 +160,7 @@ func (pow PowExpr) String() string {
 }
 
 
+// LnExpr stands for a natural logarithm expression.
 type LnExpr struct {
 	FunctionExpr
 	arg   Expression
@@ -168,6 +179,8 @@ func (ln LnExpr) wrappedLn(power sets.Number) (result sets.Number, err error) {
 }
 
 
+// Evaluate returns the natural logarithm of the evaluated inner expression's value.
+// It returns an error if the inner value is negative.
 func (ln LnExpr) Evaluate(args Arguments) (sets.Number, error) {
 	if result, err := ln.arg.Evaluate(args); err != nil {
 		return nil, err
@@ -177,6 +190,7 @@ func (ln LnExpr) Evaluate(args Arguments) (sets.Number, error) {
 }
 
 
+// Derivative uses the rule of the natural logarithm and also applies chain rule.
 func (ln LnExpr) Derivative(wrt Variable) (Expression, error) {
 	if derivative, err := ln.arg.Derivative(wrt); err != nil {
 		return nil, err
@@ -186,21 +200,27 @@ func (ln LnExpr) Derivative(wrt Variable) (Expression, error) {
 }
 
 
+// Arguments returns a list of expression just being the inner argument.
 func (ln LnExpr) Arguments() []Expression {
 	return []Expression{ ln.arg }
 }
 
 
+// CollectVariables digs into the inner expression.
 func (ln LnExpr) CollectVariables(variables Variables) {
 	ln.arg.CollectVariables(variables)
 }
 
 
+// IsConstant returns whether the inner expression is constant with respect to the given variable.
 func (ln LnExpr) IsConstant(wrt Variable) bool {
 	return ln.arg.IsConstant(wrt)
 }
 
 
+// Simplify attempts a constant simplification of the inner value.
+// If the simplified inner expression is constant, it attempts a calculation of its natural logarithm.
+// It will be an error if the inner simplified value is negative.
 func (ln LnExpr) Simplify() (Expression, error) {
 	if simplified, err := ln.arg.Simplify(); err != nil {
 		return nil, err
@@ -216,11 +236,13 @@ func (ln LnExpr) Simplify() (Expression, error) {
 }
 
 
+// String represents the natural logarithm as ln(X).
 func (ln LnExpr) String() string {
 	return FunctionDisplay(ln)
 }
 
 
+// LogExpr stands for a logarithm of any base.
 type LogExpr struct {
 	FunctionExpr
 	power   Expression
@@ -240,6 +262,8 @@ func (log LogExpr) wrappedLn(power, base sets.Number) (result sets.Number, err e
 }
 
 
+// Evaluate computes the logarithm after evaluating both the base and the power expressions.
+// It is an error if the power (or the base) evaluates a negative value.
 func (log LogExpr) Evaluate(args Arguments) (sets.Number, error) {
 	if result, err := log.power.Evaluate(args); err != nil {
 		return nil, err
@@ -251,27 +275,35 @@ func (log LogExpr) Evaluate(args Arguments) (sets.Number, error) {
 }
 
 
+// Derivative uses the generic logarithm rule of the derivative.
+// It first converts the Log(a, b) into Ln(b)/Ln(a) and then computes the derivative.
 func (log LogExpr) Derivative(wrt Variable) (Expression, error) {
 	return Mul(Ln(log.power), Inverse(Ln(log.base))).Derivative(wrt)
 }
 
 
+// Arguments returns (base, exponent) as the Log function arguments.
 func (log LogExpr) Arguments() []Expression {
 	return []Expression{ log.base, log.power }
 }
 
 
+// CollectVariables digs into the power and the base.
 func (log LogExpr) CollectVariables(variables Variables) {
 	log.power.CollectVariables(variables)
 	log.base.CollectVariables(variables)
 }
 
 
+// IsConstant returns whether both power and base are constant with respect to a given value.
 func (log LogExpr) IsConstant(wrt Variable) bool {
 	return log.base.IsConstant(wrt) && log.power.IsConstant(wrt)
 }
 
 
+// Simplify attempts simplifying both base and power expressions, and then simplifies the logarithm.
+// If both base and exponent simplify into constants, then it attempts a logarithm calculation, which
+// may fail if either of the constants is negative.
 func (log LogExpr) Simplify() (Expression, error) {
 	if simplifiedBase, err := log.base.Simplify(); err != nil {
 		return nil, err
@@ -295,17 +327,20 @@ func (log LogExpr) Simplify() (Expression, error) {
 }
 
 
+// String represents the logarithm as log(a, b).
 func (log LogExpr) String() string {
 	return FunctionDisplay(log)
 }
 
 
+// ExpExpr stands for an expression like e^X.
 type ExpExpr struct {
 	FunctionExpr
 	exponent Expression
 }
 
 
+// Derivative computes the rule of e^X, also applying chain rule.
 func (exp ExpExpr) Derivative(wrt Variable) (Expression, error) {
 	if derivative, err := exp.exponent.Derivative(wrt); err != nil {
 		return nil, err
@@ -315,6 +350,7 @@ func (exp ExpExpr) Derivative(wrt Variable) (Expression, error) {
 }
 
 
+// Evaluate computes the value of the inner expression (the exponent) and then computes e^(that value).
 func (exp ExpExpr) Evaluate(args Arguments) (sets.Number, error) {
 	if result, err := exp.exponent.Evaluate(args); err != nil {
 		return nil, err
@@ -324,21 +360,25 @@ func (exp ExpExpr) Evaluate(args Arguments) (sets.Number, error) {
 }
 
 
+// Arguments returns a list of expressions only including the exponent.
 func (exp ExpExpr) Arguments() []Expression {
 	return []Expression{ exp.exponent }
 }
 
 
+// CollectVariables dig into the exponent expression.
 func (exp ExpExpr) CollectVariables(variables Variables) {
 	 exp.exponent.CollectVariables(variables)
 }
 
 
+// IsConstant returns whether the exponent expression is constant with respect to the given variable.
 func (exp ExpExpr) IsConstant(wrt Variable) bool {
 	return exp.exponent.IsConstant(wrt)
 }
 
 
+// Simplify attempts a constant evaluation of the exponent, and then of e^(exponent).
 func (exp ExpExpr) Simplify() (Expression, error) {
 	if simplified, err := exp.exponent.Simplify(); err != nil {
 		return nil, err
@@ -350,26 +390,31 @@ func (exp ExpExpr) Simplify() (Expression, error) {
 }
 
 
+// String represents this expression as exp(X).
 func (exp ExpExpr) String() string {
 	return FunctionDisplay(exp)
 }
 
 
+// Pow constructs a power expression with base and exponent.
 func Pow(base, exponent Expression) Expression {
 	return PowExpr{base, exponent}
 }
 
 
+// Ln constructs a natural logarithm operation expression.
 func Ln(power Expression) Expression {
 	return LnExpr{FunctionExpr{"ln"}, power}
 }
 
 
+// Log constructs a logarithm expression with base and power.
 func Log(base, power Expression) Expression {
 	return LogExpr{FunctionExpr{"log"}, power, base}
 }
 
 
+// Exp constructs an e^X expression.
 func Exp(exponent Expression) Expression {
 	return ExpExpr{FunctionExpr{"exp"}, exponent}
 }
