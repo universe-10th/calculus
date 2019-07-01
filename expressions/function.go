5046dd56ceda10efa4006525690a104e95317b29
luismasuelli <luis@triplemint.com>
2019-06-30 22:39:40 -0500

    Function documentation.

--- expressions/function.go
+++ expressions/function.go
@@ -6,6 +6,8 @@ import (
 )
 
 
+// Function interface requires an implementation of standard name and arguments methods.
+// Standard name is, e.g., "Ln" while arguments is an array of the inner involved expressions.
 type Function interface {
 	fmt.Stringer
 	StandardName() string
@@ -13,6 +15,8 @@ type Function interface {
 }
 
 
+// FunctionExpr is a base structure for other function nodes. It is also marked as self-contained,
+// in the same way of variables and constants.
 type FunctionExpr struct {
 	standardName string
 }
@@ -23,6 +27,7 @@ func (function FunctionExpr) IsSelfContained() bool {
 }
 
 
+// The function's standard name, e.g. Ln, Sin, Tan.
 func (function FunctionExpr) StandardName() string {
 	return function.standardName
 }
@@ -33,6 +38,7 @@ func (function FunctionExpr) Arguments() []Expression {
 }
 
 
+// FunctionDisplay appropriately represents a function object as a human-readable string.
 func FunctionDisplay(function Function) string {
 	arguments := function.Arguments()
 	placeholders := make([]string, len(arguments))
