770962565b2862702a20b251703f05f1c316513b
luismasuelli <luis@triplemint.com>
2019-07-01 12:13:36 -0500

    Changing corrections in NR method

--- solvers/newton_raphson.go
+++ solvers/newton_raphson.go
@@ -5,15 +5,61 @@ import (
 	"github.com/universe-10th-calculus/expressions"
 	"github.com/universe-10th-calculus/solvers/support"
 	"github.com/universe-10th-calculus/sets"
+	"errors"
+	"math/rand"
+	"time"
 )
+
+
+var ErrMaxNewtonRaphsonArgCorrections = errors.New("could not correct the argument to avoid a zero derivative")
+
+
+func findNewtonRaphsonValidArg(expression, derivative expressions.Expression, variable expressions.Variable,
+	                           currentRes, currentDerRes sets.Number, currentImg, currentDerImg, quot *big.Float,
+	                           arg, epsilon, delta, random *big.Float, maxArgCorrections uint32) (*big.Float, error) {
+	var err error
+	var current = arg
+	var recalculate = false
+	// This will proceed as follows: it will try calculating the derivative a lof of times until the result is
+	// not zero. Otherwise, successive steps will modify the current x by a very small random step. When the
+	// derivative is found, the function value is recalculated (if failed at least once), the quotient is computed,
+	// and the new value for the argument is corrected.
+	for tryOut := uint32(0); tryOut < maxArgCorrections; tryOut++ {
+		if currentDerRes, err = derivative.Evaluate(expressions.Arguments{variable: current}); err != nil {
+			return nil, err
+		}
+		if currentDerImg, err = support.ForceFloat(currentDerRes); err != nil {
+			return nil, err
+		}
+		if currentDerImg.Sign() != 0 {
+			// evaluate f(currentArg) into currentImg if told to recalculate.
+			if recalculate {
+				if currentRes, err = expression.Evaluate(expressions.Arguments{variable: current}); err != nil {
+					return nil, err
+				}
+				if currentImg, err = support.ForceFloat(currentRes); err != nil {
+					return nil, err
+				}
+			}
+			current.Sub(current, quot.Quo(currentImg, currentDerImg))
+			return current, nil
+		} else {
+			recalculate = true
+			random.SetFloat64(rand.Float64())
+			current.Add(current, delta.Mul(epsilon, random))
+		}
+	}
+	return nil, ErrMaxNewtonRaphsonArgCorrections
+}
 
 
 // NewtonRaphson tries a Newton-Raphson iteration to find the nearest approximate root of the given function.
 // If you want to find x so that f(x) == target, just define g(x) = f(x) - target and you will be on your way.
 // While the usual newton-raphson fails when f'(x) (or g'(x)) is zero, we replace such value by the same epsilon
-// in the tolerance.
+// in the tolerance. The number of max iterations, and the number of max corrections of the argument when the
+// derivative is zero must be provided as well.
 func NewtonRaphson(expression expressions.Expression, initialGuess, epsilon *big.Float,
-	               maxIterations uint32) (result *big.Float, exception error) {
+	               maxIterations, maxArgCorrectionsPerIteration uint32) (result *big.Float, exception error) {
 	var derivative expressions.Expression
 	var variable expressions.Variable
 	var err error
@@ -26,7 +72,12 @@ func NewtonRaphson(expression expressions.Expression, initialGuess, epsilon *big
 	var diff = big.NewFloat(0)
 	var dist = big.NewFloat(0)
 	var quot = big.NewFloat(0)
-	if variable, err = support.GetTheOnlyVariable(expression); err != nil {
+	var delta = big.NewFloat(0)
+	var random = big.NewFloat(0)
+	rand.Seed(time.Now().UTC().UnixNano())
+	if expression, err = expression.Simplify(); err != nil {
+		return nil, err
+	} else if variable, err = support.GetTheOnlyVariable(expression); err != nil {
 		return nil, err
 	} else if derivative, err = expression.Derivative(variable); err != nil {
 		return nil, err
@@ -48,22 +99,13 @@ func NewtonRaphson(expression expressions.Expression, initialGuess, epsilon *big
 			if support.CloseTo(currentImg, zero, diff, dist, epsilon) {
 				return currentArg, nil
 			}
-			// Calculate (and perhaps correct) the derivative at that value.
-			if currentDerRes, err = derivative.Evaluate(expressions.Arguments{variable: currentArg}); err != nil {
+			// Otherwise, process a newton-raphson step.
+			if currentArg, err = findNewtonRaphsonValidArg(
+				expression, derivative, variable, currentRes, currentDerRes, currentImg, currentDerImg, quot,
+				currentArg, epsilon, delta, random, maxArgCorrectionsPerIteration,
+			); err != nil {
 				return nil, err
 			}
-			if currentDerImg, err = support.ForceFloat(currentDerRes); err != nil {
-				return nil, err
-			}
-			// Correct the derivative if zero. Use epsilon instead.
-			// TODO This is wrong. If the derivative is zero, one has to add the epsilon to X instead,
-			// TODO   and then attempt again a calculation of f(x) and g(x), repeating this process until
-			// TODO   a derivative != 0 is found.
-			if currentDerImg.Sign() == 0 {
-				currentDerImg = epsilon
-			}
-			// Compute nextArg = currentArg - func/der.
-			currentArg.Sub(currentArg, quot.Quo(currentImg, currentDerImg))
 		}
 		return nil, support.ErrIterationsExhausted
 	}
