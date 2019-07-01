912d747fffa271dac4968b4a358fd6b3745db5dc
luismasuelli <luis@triplemint.com>
2019-06-30 22:36:37 -0500

    Factorial documentation.

--- expressions/factorial.go
+++ expressions/factorial.go
@@ -8,11 +8,18 @@ import (
 	"fmt"
 )
 
+
+// FactorialExpr is a factorial expression. It only makes sense with numbers in N0 set.
+// Factorial takes a single inner expression which MUST evaluate into N0.
 type FactorialExpr struct {
 	arg Expression
 }
 
 
+// Derivative returns a 0 constant expression, or an error, when attempting the derivative.
+// This means: a 0 constant expression will be returned if the inner expression is constant
+// with respect to the variable, or an error if it is not: factorial is not a derivable
+// expression since it is not defined on R.
 func (factorial FactorialExpr) Derivative(wrt Variable) (Expression, error) {
 	// This expression is not derivable!!! must not include the
 	// variable in the wrt parameter.
@@ -26,16 +33,21 @@ func (factorial FactorialExpr) Derivative(wrt Variable) (Expression, error) {
 }
 
 
+// CollectVariables digs into the factorial's inner terms.
 func (factorial FactorialExpr) CollectVariables(variables Variables) {
 	factorial.arg.CollectVariables(variables)
 }
 
 
+// IsConstant will return whether the inner expression is constant with respect to the given variable.
 func (factorial FactorialExpr) IsConstant(wrt Variable) bool {
 	return factorial.arg.IsConstant(wrt)
 }
 
 
+// Simplify simplifies the inner expression and, if the simplified inner expression is constant,
+// generates a new constant expression with the factorial of the returned constant's value.
+// It will be an error if the inner constant does not evaluate into N0.
 func (factorial FactorialExpr) Simplify() (Expression, error) {
 	if simplified, err := factorial.arg.Simplify(); err != nil {
 		return nil, err
@@ -62,6 +74,8 @@ func (factorial FactorialExpr) wrappedFactorial(input sets.Number) (result sets.
 }
 
 
+// Evaluate computes the factorial over the evaluated inner argument's value.
+// It will be an error if the inner value does not evaluate into N0.
 func (factorial FactorialExpr) Evaluate(args Arguments) (sets.Number, error) {
 	if result, err := factorial.arg.Evaluate(args); err != nil {
 		return nil, err
@@ -71,6 +85,7 @@ func (factorial FactorialExpr) Evaluate(args Arguments) (sets.Number, error) {
 }
 
 
+// String represents the factorial as x! or (x)! appropriately.
 func (factorial FactorialExpr) String() string {
 	if _, ok := factorial.arg.(SelfContained); ok {
 		return fmt.Sprintf("%s!", factorial.arg)
@@ -85,6 +100,7 @@ func (factorial FactorialExpr) IsSelfContained() bool {
 }
 
 
+// Factorial constructs a new factorial node with the given inner term.
 func Factorial(value Expression) Expression {
 	return FactorialExpr{value}
 }
