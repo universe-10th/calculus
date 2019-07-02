88340e1dcbcabad5909ed0e083f3b26dc79e0895
luismasuelli <luis@triplemint.com>
2019-07-02 17:27:40 -0500

    Adding documentation to GoalBasedExpression

--- utils/goal.go
+++ utils/goal.go
@@ -7,6 +7,7 @@ import (
 )
 
 
+// Takes an existing expression -say- f(...) and generates its g(...) = f(...) - goal.
 func GoalBasedExpression(expression expressions.Expression, goal interface{}) expressions.Expression {
 	wrapped, _ := sets.Wrap(goal)
 	return expressions.Add(expression, expressions.Num(ops.Neg(wrapped)))
