13b4012e459c2841f45519f07ba6dd51f6dd0ba6
luismasuelli <luis@triplemint.com>
2019-07-04 17:22:19 -0500

    Changing solver precision

--- samples/models/main.go
+++ samples/models/main.go
@@ -33,7 +33,7 @@ func main() {
 		X: 1, Y: 2, Z: 3,
 	}.Wrap(), nil)
 	fmt.Printf("Return after evaluating W=Model^-1(X: 1, Y: 2, Z: 3): %v %v\n", result, err)
-	nrSolver := solvers.MakeNewtonRaphsonSolver(big.NewFloat(0).SetInt64(0), support.Epsilon(10), 100, 100)
+	nrSolver := solvers.MakeNewtonRaphsonSolver(big.NewFloat(0).SetInt64(1000000), support.Epsilon(7), 100, 100)
 	result, err = model.Evaluate(Arguments{
 		W: 1, Y: 2, Z: 3,
 	}.Wrap(), nrSolver)
