8cfb003cdb0e7c143ec64bbee8d18215d0ea72b9
luismasuelli <luis@triplemint.com>
2019-08-23 17:45:04 -0500

    Adding the functions to the contract

--- models/computable.go
+++ models/computable.go
@@ -6,4 +6,6 @@ import "github.com/universe-10th/calculus/expressions"
 // This interface is intended for flows and flow chains.
 type Computable interface {
 	Compute(arguments expressions.Arguments) (expressions.Arguments, error)
+	Input() expressions.Variables
+	Output() expressions.Variables
 }
