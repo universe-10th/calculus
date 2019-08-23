aa14429c5f8c272a32c9081b14c381f9646dd864
luismasuelli <luis@triplemint.com>
2019-08-23 18:18:39 -0500

    Adding an error for the computables

--- models/computable.go
+++ models/computable.go
@@ -1,6 +1,9 @@
 package models
 
-import "github.com/universe-10th/calculus/expressions"
+import (
+	"github.com/universe-10th/calculus/expressions"
+	"errors"
+)
 
 
 // This interface is intended for flows and flow chains.
@@ -10,3 +13,6 @@ type Computable interface {
 	Input() expressions.Variables
 	Output() expressions.Variables
 }
+
+
+var ErrComputableIsNil = errors.New("a given computable is nil")
