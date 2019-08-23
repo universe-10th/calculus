c48065cc19f46423817b26c91f21d88fce5c658c
luismasuelli <luis@triplemint.com>
2019-08-23 17:38:20 -0500

    Flow and Flow chains parent interface.

---
+++ models/computable.go
@@ -0,0 +1,9 @@
+package models
+
+import "github.com/universe-10th/calculus/expressions"
+
+
+// This interface is intended for flows and flow chains.
+type Computable interface {
+	Compute(arguments expressions.Arguments) (expressions.Arguments, error)
+}
