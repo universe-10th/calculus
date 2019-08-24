6864020f71393721aef921497be29d00005fcca6
luismasuelli <luis@triplemint.com>
2019-08-23 19:44:08 -0500

    Changing signature of model evaluation

--- samples/models/main.go
+++ samples/models/main.go
@@ -24,24 +24,25 @@ func SampleModel() *utils.Model {
 func main() {
 	model := SampleModel()
 	var result interface{}
+	var variable Variable
 	var err    error
-	result, err = model.Evaluate(Arguments{
+	result, variable, err = model.Evaluate(Arguments{
 		X: 1, Y: 2, W: 3,
 	}.Wrap(), nil)
-	fmt.Printf("Return after evaluating Z=Model(X: 1, Y: 2, W: 3): %v %v\n", result, err)
-	result, err = model.Evaluate(Arguments{
+	fmt.Printf("Return after evaluating Z=Model(X: 1, Y: 2, W: 3): %v=%v %v\n", variable, result, err)
+	result, variable, err = model.Evaluate(Arguments{
 		X: 1, Y: 2, Z: 3,
 	}.Wrap(), nil)
-	fmt.Printf("Return after evaluating W=Model^-1(X: 1, Y: 2, Z: 3): %v %v\n", result, err)
+	fmt.Printf("Return after evaluating W=Model^-1(X: 1, Y: 2, Z: 3): %v=%v %v\n", variable, result, err)
 	nrSolver := solvers.MakeNewtonRaphsonSolver(big.NewFloat(0).SetInt64(1000000), support.Epsilon(7), 100, 100)
-	result, err = model.Evaluate(Arguments{
+	result, variable, err = model.Evaluate(Arguments{
 		W: 1, Y: 2, Z: 3,
 	}.Wrap(), nrSolver)
-	fmt.Printf("Return after evaluating X=Model^-1(W: 1, Y: 2, Z: 3): %v %v\n", result, err)
-	result, err = model.Evaluate(Arguments{
+	fmt.Printf("Return after evaluating X=Model^-1(W: 1, Y: 2, Z: 3): %v=%v %v\n", variable, result, err)
+	result, variable, err = model.Evaluate(Arguments{
 		X: 1, W: 2, Z: 3,
 	}.Wrap(), nrSolver)
 	// It WILL fail with Newton-Raphson, since the function is not quadratically convergent.
 	// It will try more and more bigger negative values for Y, and then will return derivative of 0 due to lack of precision.
-	fmt.Printf("Return after evaluating Y=Model^-1(X: 1, W: 2, Z: 3): %v %v\n", result, err)
+	fmt.Printf("Return after evaluating Y=Model^-1(X: 1, W: 2, Z: 3): %v=%v %v\n", variable, result, err)
 }
